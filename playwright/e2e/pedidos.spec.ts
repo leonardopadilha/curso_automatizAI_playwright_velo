e82006602fd65fe1d37ca4a581e98007e57f18d9
Leonardo Padilha <leonardopadilha28@gmail.com>
2026-03-17 21:29:29 -0300

    Adicionando cenário para validar pedido em análise

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -137,6 +137,53 @@ test.describe('Consulta de pedido', () => {
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
     `);
   })
+
+  test('deve consultar um pedido em análise - Validação com toMatchAriaSnapshot', async ({ page }) => {
+    // Test Data
+    const order = {
+      number: 'VLO-YSJDBU',
+      status: 'EM_ANALISE',
+      color: 'Lunar White',
+      Wheels: 'aero Wheels',
+      customer: {
+        name: 'Joao da Silva',
+        email: 'joao@velo.dev'
+      },
+      payment: 'À Vista'
+    }
+ 
+   await page.getByPlaceholder('Ex: VLO-ABC123').fill(order.number)
+   await page.getByTestId('search-order-button').click()
+
+   await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.Wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `);
+  })
   
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
     const order = generateOrderCode()
