ed3f27f23f6ca087de74b4792c9c76495c38ec89
Leonardo Padilha <leonardopadilha28@gmail.com>
2026-04-09 06:42:51 -0300

    Validando a mudança da imagem e das rodas

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../support/fixtures'
 
-test.skip('webapp deve estar online', async ({ page }) => {
+test('webapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5173')
 
   await expect(page).toHaveTitle(/Velô by Papito/)
